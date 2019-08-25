9cc56e99e4adbb2d80000a5ebfb9aaebf766407b
Your Name <luisgust.123@gmail.com>
2019-08-25 10:48:10 -0500

    Comunicacion entre componentes hijo-padre (EventEmitter)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombrePerro = 'Firulais';
+  mensaje : string;
+
+  recibirLadrido(e : any){
+    this.mensaje = e;
+  }
 }
--- src/app/perro/perro.component.ts
+++ src/app/perro/perro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-perro',
@@ -9,10 +9,16 @@ export class PerroComponent implements OnInit {
 
   @Input()
   nombre: string;
-
+  @Output()
+  emisor = new EventEmitter();
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ladrar(){
+    this.emisor.emit('guau!')
   }
 
 }
